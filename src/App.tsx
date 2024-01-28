dbacec85c969d5f07d7e684a07e8324cbd13f72f
farooq-teqniqly <farooq@teqniqly.com>
2024-01-28 11:01:06 -0800

    First

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { Outlet } from "react-router-dom";
-
 function App() {
   return (
-    <div className={`container mx-auto`}>
-      <div className={`flex flex-col p-10 text-center text-xl`}>
-        <Outlet></Outlet>
-      </div>
+    <div>
+      <h1>Pro Shop</h1>
     </div>
   );
 }
