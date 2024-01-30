5456c14ec955376e7f57735c155c69539ebba8c9
farooq-teqniqly <farooq@teqniqly.com>
2024-01-29 17:02:33 -0800

    Component test refactor

--- src/components/Footer.cy.tsx
+++ src/components/Footer.cy.tsx
@@ -2,7 +2,7 @@ import Footer from "./Footer";
 import { assertTextExact } from "../../cypress/support/utilities";
 
 describe("<Footer />", () => {
-  it("renders", () => {
+  it("renders copyright text with current year", () => {
     cy.mount(<Footer />);
     const year = new Date().getFullYear();
     assertTextExact("footer", `Copyright © Pro Shop ${year}`);
--- src/components/Header.cy.tsx
+++ src/components/Header.cy.tsx
@@ -2,13 +2,22 @@ import Header from "./Header";
 import { assertText, assertClass, assertTestId } from "../../cypress/support/utilities";
 
 describe("<Header />", () => {
-  it("renders", () => {
+  beforeEach(() => {
     cy.mount(<Header />);
+  });
+
+  it("renders menu items", () => {
     assertText("header", "Pro Shop");
     assertText("header", "Cart");
     assertText("header", "Sign In");
+  });
+
+  it("renders menu icons", () => {
     assertClass("svg", "fa-cart-shopping");
     assertClass("svg", "fa-user");
+  });
+
+  it("renders logo", () => {
     assertTestId("logo");
   });
 });
