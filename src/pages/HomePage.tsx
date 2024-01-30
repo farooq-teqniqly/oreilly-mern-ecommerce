dbb3c3b92d709e20e1c052178960f8137faf8b50
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-01-30 10:41:34 -0800

    Show product list on home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import { Container } from "react-bootstrap";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ProductList from "src/components/ProductList";
+import products from "src/data/products";
 
 function HomePage() {
   return (
@@ -9,6 +11,7 @@ function HomePage() {
       <main className="py-3">
         <Container>
           <h1>Welcome to Pro Shop</h1>
+          <ProductList products={products} />
         </Container>
       </main>
       <Footer />
